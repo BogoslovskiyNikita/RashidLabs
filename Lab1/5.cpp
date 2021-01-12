448e20915ac5eac5926b854c0d1a31457342e122
BogoslovskiyNikita <65765163+BogoslovskiyNikita@users.noreply.github.com>
2021-01-12 19:33:58 +0300

    Add files via upload

---
+++ Lab1/5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+main() {
+    /*инциализация переменных*/
+    int a = 10;
+    int b = 20;
+    int temp;
+
+    /*обмен значениями*/
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}
