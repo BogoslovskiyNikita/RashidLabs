38f3467a04988735700629d450f77bde4ea761d1
BogoslovskiyNikita <65765163+BogoslovskiyNikita@users.noreply.github.com>
2021-01-14 00:51:59 +0300

    Add files via upload

---
+++ Lab1/11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int min, max;
+    cin >> min;
+    cin >> max;
+    cout << (rand() % (max - 1)) + min << + "\n";
+    cout << (rand() % (max - 1)) + min << + "\n";
+    cout << (rand() % (max - 1)) + min;
+    return 0;
+}
