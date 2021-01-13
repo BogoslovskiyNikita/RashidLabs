a2834d9768e59bc8250e74315b8b3251d6ac3a55
BogoslovskiyNikita <65765163+BogoslovskiyNikita@users.noreply.github.com>
2021-01-14 01:07:13 +0300

    Add files via upload

---
+++ Lab1/6.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    switch (num)
+    {
+        case 2:
+            cout << "10";
+            break;
+        case 3:
+            cout << "11";
+            break;
+        case 4:
+            cout << "100";
+            break;
+        case 5:
+            cout << "101";
+            break;
+        case 6:
+            cout << "110";
+            break;
+        case 7:
+            cout << "111";
+            break;
+        case 8:
+            cout << "1000";
+            break;
+        case 9:
+            cout << "1001";
+            break;
+        case 10:
+            cout << "1010";
+            break;
+        case 11:
+            cout << "1011";
+            break;
+        case 12:
+            cout << "1100";
+            break;
+        case 13:
+            cout << "1101";
+            break;
+        case 14:
+            cout << "1110";
+            break;
+        case 15:
+            cout << "1111";
+            break;
+        case 16:
+            cout << "10000";
+            break;
+    
+    }
+    
+}
